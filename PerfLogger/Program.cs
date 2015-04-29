ed4ec19830c8349d1b6471b7d3be06a9568d54d0
Pavel Egorov <xoposhiy@mail.ru>
2015-04-29 18:19:00 +0500

    fixes in slides

--- PerfLogger/Program.cs
+++ PerfLogger/Program.cs
@@ -9,9 +9,9 @@ namespace PerfLogger
 		{
 			/*
 			var sum = 0.0;
-			using (PerfLogger.Measure("100M for iterations"))
+			using (PerfLogger.Measure(t => Console.WriteLine("for: {0}", t)))
 				for (var i = 0; i < 100000000; i++) sum += i;
-			using (PerfLogger.Measure("100M LINQ iterations"))
+			using (PerfLogger.Measure(t => Console.WriteLine("linq: {0}", t)))
 				sum -= Enumerable.Range(0, 100000000).Sum(i => (double)i);
 			Console.WriteLine(sum);
 			*/
