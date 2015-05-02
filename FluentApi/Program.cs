318ff70824a9a20c2180c6218738b8cdd8fd786c
valertron <valertron@skbkontur.ru>
2015-05-02 12:17:19 +0500

    PerfLogger implementation

--- FluentApi/Program.cs
+++ FluentApi/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace FluentTask
 {
@@ -6,9 +10,8 @@ namespace FluentTask
 	{
 		private static void Main()
 		{
-
-/*
-			var behaviour = new Behavior()
+			var acts = new List<Action>();
+			var behaviour = new Behavior(acts)
 				.Say("Привет мир!")
 				.UntilKeyPressed(b => b
 					.Say("Ля-ля-ля!")
@@ -21,10 +24,73 @@ namespace FluentTask
 				.Delay(TimeSpan.FromSeconds(1))
 				.Say("Кто здесь?!")
 				.Delay(TimeSpan.FromMilliseconds(2000));
-
 			behaviour.Execute();
-*/
 
+			Console.ReadLine();
 		}
+	}
+
+	interface IBehavior
+	{
+		Behavior Say(string input);
+		Behavior Delay(TimeSpan time);
+		Behavior UntilKeyPressed(Func<Behavior, Behavior> myFunc);
+		Behavior Jump(JumpHeight jumpHeight);
+		void Execute();
+	}
+
+	internal class Behavior : IBehavior
+	{
+
+		private readonly List<Action> actions;
+
+		public Behavior(IEnumerable<Action> actions)
+		{
+			this.actions = actions.ToList();
+		}
+
+		public void Execute()
+		{
+			foreach (var action in actions)
+			{
+				action();
+			}
+		}
+
+		public Behavior Say(string input)
+		{
+			actions.Add(() => Console.WriteLine(input));
+			return new Behavior(actions);
+		}
+
+		public Behavior Delay(TimeSpan time)
+		{
+			actions.Add(() => Thread.Sleep(time));
+			return new Behavior(actions);
+		}
+
+		public Behavior UntilKeyPressed(Func<Behavior, Behavior> myFunc)
+		{
+			actions.Add(() =>
+			{
+
+				var beh = new Behavior(new List<Action>());
+				while (Console.KeyAvailable == false)
+				{
+					myFunc(beh);
+					beh.Execute();
+					Thread.Sleep(250); // Loop until input is entered.
+				}
+			});
+			return new Behavior(actions);
+		}
+
+		public Behavior Jump(JumpHeight jumpHeight)
+		{
+				actions.Add(() => Console.ReadKey(true));
+
+			return new Behavior(actions);
+		}
+
 	}
 }
--- PerfLogger/Program.cs
+++ PerfLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Diagnostics;
 
 namespace PerfLogger
 {
@@ -7,14 +8,44 @@ namespace PerfLogger
 	{
 		static void Main(string[] args)
 		{
-			/*
+			var perf = new PerfLogger();
 			var sum = 0.0;
 			using (PerfLogger.Measure(t => Console.WriteLine("for: {0}", t)))
 				for (var i = 0; i < 100000000; i++) sum += i;
+
 			using (PerfLogger.Measure(t => Console.WriteLine("linq: {0}", t)))
 				sum -= Enumerable.Range(0, 100000000).Sum(i => (double)i);
 			Console.WriteLine(sum);
-			*/
+
+			Console.ReadLine();
 		}
+
+		public class PerfLogger: IDisposable
+		{
+			private static Action<double> Action;
+			private static Stopwatch stopwatch;
+			public static PerfLogger instance;
+			public PerfLogger()
+			{
+				instance = this;
+			}
+
+			public static PerfLogger Measure(Action<double> action)
+			{
+				stopwatch = new Stopwatch();
+				stopwatch.Start();
+				Action = action;
+				return instance;
+			}
+
+			public void Dispose()
+			{
+				stopwatch.Stop();
+				var elps = stopwatch.Elapsed.TotalMilliseconds;
+				Action(elps);
+			}
+		}
+		
+		
 	}
 }
